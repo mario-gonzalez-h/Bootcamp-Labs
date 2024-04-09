057a8357f53ed94e14982058f8ffa361115d9b63
Magh98 <magh98@outlook.com>
2024-04-09 15:56:18 -0500

    first commit

---
+++ BasicCalculator/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //The two floating point numbers which the user will utilize
+        float firstNumber = 5;
+        float secondNumber = 12;
+
+        System.out.println("Possible Calculations:\n(A)dd\n(S)ubstract\n(M)ultiply\n(D)ivide");
+//        Scanner scanner = new Scanner(System.in);
+//        Char selection = scanner.nextcha
+
+    }
+}gf
