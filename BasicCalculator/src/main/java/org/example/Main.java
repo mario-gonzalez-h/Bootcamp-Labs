7b23a8cf3de0a90cd4245b3f8234ce0bbd7ce91a
Magh98 <magh98@outlook.com>
2024-04-11 13:18:43 -0500

    Function commit

--- BasicCalculator/src/main/java/org/example/Main.java
+++ BasicCalculator/src/main/java/org/example/Main.java
@@ -1,14 +1,64 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //The two floating point numbers which the user will utilize
-        float firstNumber = 5;
-        float secondNumber = 12;
+        // Asking the user for their input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Possible Calculations:\n(A)dd\n(S)ubstract\n(M)ultiply\n(D)ivide\nPlease select an option: ");
+        String selection = scanner.nextLine();
+        // The two integers numbers which the user will utilize
+        System.out.println("Please enter your first number: ");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Please enter your second number: ");
+        int secondNumber = scanner.nextInt();
 
-        System.out.println("Possible Calculations:\n(A)dd\n(S)ubstract\n(M)ultiply\n(D)ivide");
-//        Scanner scanner = new Scanner(System.in);
-//        Char selection = scanner.nextcha
+        // Switch statement to select operation
+        switch (selection.toLowerCase()) {
+            case "a":
+                addition(firstNumber, secondNumber);
+                break;
+            case "s":
+                subtraction(firstNumber, secondNumber);
+                break;
+            case "m":
+                multiplication(firstNumber, secondNumber);
+                break;
+            case "d":
+                division(firstNumber, secondNumber);
+                break;
+            default:
+                System.out.println("Invalid selection. Please choose A, S, M, or D.");
+                break;
+        }
+    }
+
+    // Function for addition
+    public static void addition(int firstNumber, int secondNumber){
+        int result = firstNumber + secondNumber;
+        System.out.print("Your result is: " + result);
+    }
+
+    // Function for subtraction
+    public static void subtraction(int firstNumber, int secondNumber){
+        int result = firstNumber - secondNumber;
+        System.out.print("Your result is: " + result);
 
     }
-}gf
+
+    // Function for multiplication
+    public static void multiplication(int firstNumber, int secondNumber){
+        int result = firstNumber * secondNumber;
+        System.out.print("Your result is: " + result);
+    }
+
+    // Function for division
+    public static void division(int firstNumber, int secondNumber){
+        if (secondNumber != 0) {
+            int result = firstNumber / secondNumber;
+            System.out.print("Your result is: " + result);
+        } else {
+            System.out.println("Error: Cannot divide by zero.");
+        }
+    }
+}
